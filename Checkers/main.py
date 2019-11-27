b88729ad49fd970db4669e15eba409d74a25b0ba
Omer Khan <okhanlodhi@gmail.com>
2019-11-27 00:13:50 -0600

    tkinter gui testing

--- Checkers/main.py
+++ Checkers/main.py
@@ -2,7 +2,7 @@ width = int(8)
 height = int(8)
 grid = []
 row = []
-bak = "."
+bak = 0
 
 for i in range (width):
     row.append(bak)
@@ -10,7 +10,34 @@ for i in range (width):
 for i in range (height):
     grid.append(row)
 
+#Player One = 1
+grid[0][1] = 1
+grid[0][3] = 1
+grid[0][5] = 1
+grid[0][7] = 1
+grid[1][0] = 1
+grid[1][2] = 1
+grid[1][4] = 1
+grid[1][6] = 1
+grid[2][1] = 1
+grid[2][3] = 1
+grid[2][5] = 1
+grid[2][7] = 1
+
+#Player Two = 2
+grid[5][0] = 1
+grid[5][2] = 1
+grid[5][4] = 1
+grid[5][6] = 1
+grid[6][1] = 1
+grid[6][3] = 1
+grid[6][5] = 1
+grid[6][7] = 1
+grid[7][0] = 1
+grid[7][2] = 1
+grid[7][4] = 1
+grid[7][6] = 1
+
 for i in range (len(grid)):
     print(grid[i])
-
-
+    print("\n")
